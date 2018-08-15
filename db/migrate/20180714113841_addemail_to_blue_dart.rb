fb0f313f3acccb25f419b8889c9953bcf90a8aed
Ronak Jain <ronak.jain2116@gmail.com>
2018-08-15 17:45:48 +0530

    15svsdf

--- db/migrate/20180714113841_addemail_to_blue_dart.rb
+++
@@ -1,6 +0,0 @@
-# add email to bluedart
-class AddemailToBlueDart < ActiveRecord::Migration[5.1]
-  def change
-    add_column :blue_darts, :email, :string
-  end
-end
--- db/migrate/20180716031154_addmail_to_blue_dart.rb
+++
@@ -1,4 +0,0 @@
-class AddmailToBlueDart < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
